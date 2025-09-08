161bf5589aaad6d49bfd7193fc0b3dbe831fa6d5
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-09-08 10:47:31 +0200

    Add hardcoded movie dataset and implement processing pipeline

--- index.js
+++ index.js
@@ -24,3 +24,23 @@ const pipe = (...fns) =>
 
 const compose = (...fns) =>
     (input) => fns.reduceRight((act, fn) => fn(acc), input);
+
+// HARD CODED EXAMPLE DATA
+
+const movies = [
+    { id: 1, title: "Inception", genre: "SciFi", year: 2010, ratings: [9, 8, 9, 7, 8] },
+    { id: 2, title: "The Matrix", genre: "SciFi", year: 1999, ratings: [9, 8, 9, 7, 8] },
+    { id: 3, title: "Titanic", genre: "Romance", year: 1997, ratings: [7, 8, 6, 7, 8] },
+    { id: 4, title: "Avatar", genre: "SciFi", year: 2009, ratings: [8, 7, 8, 9, 7] },
+];
+
+// EXAMPLE PIPELINE FOR PROCESSING MOVIES
+
+const processMovies = pipe(
+    (ms) => ms.map(addAverageRating),
+    (ms) => filterByGenre("SciFi", ms),
+    (ms) => filterByYear("2000", ms),
+    sortByRating
+);
+
+console.log("Results: ", processMovies(movies));
