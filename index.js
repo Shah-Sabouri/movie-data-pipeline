7956d705a3a768f435e1652ff55b46e9b0b0ba49
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-09-08 10:28:48 +0200

    Added pure function codes in index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// PURE FUNCTIONS
+
+const calculateAverage = (ratings) =>
+    ratings.reduce((sum, r) => sum + r, 0) / ratings.length;
+
+const addAverageRating = (movie) => ({
+    ...movie,
+    averageRating: calculateAverage(movie.ratings),
+});
+
+const filterByGenre = (genre, movies) =>
+    movies.filter((movie) => movie.genre === genre);
+
+const filterByYear = (minYear, movies) =>
+    movies.filter((movie) => movie.year >= minYear);
+
+const sortByRating = (movies) =>
+    [...movies].sort((a, b) => b.averageRating - a.averageRating);
