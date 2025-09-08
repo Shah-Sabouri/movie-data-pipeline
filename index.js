4c8c3645ac8cd5b90350b62d03d5fe78c815dd71
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-09-08 11:01:16 +0200

    Move movie dataset into separate movies.js module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const movies = require("./movies");
+
 // ------------------ PURE FUNCTIONS -----------------------
 
 // To calculate average rating
@@ -33,17 +35,6 @@ const pipe = (...fns) =>
 // Same thing here but in opposite
 const compose = (...fns) =>
     (input) => fns.reduceRight((act, fn) => fn(acc), input);
-
-// ---------------------------------------------------------
-
-// HARD CODED EXAMPLE DATA
-
-const movies = [
-    { id: 1, title: "Inception", genre: "SciFi", year: 2010, ratings: [9, 8, 9, 7, 8] },
-    { id: 2, title: "The Matrix", genre: "SciFi", year: 1999, ratings: [9, 8, 9, 7, 8] },
-    { id: 3, title: "Titanic", genre: "Romance", year: 1997, ratings: [7, 8, 6, 7, 8] },
-    { id: 4, title: "Avatar", genre: "SciFi", year: 2009, ratings: [8, 7, 8, 9, 7] },
-];
 
 // ---------------------------------------------------------
 
---
+++ movies.js
@@ -0,0 +1,12 @@
+// HARD CODED EXAMPLE DATA
+
+const movies = [
+    { id: 1, title: "Inception", genre: "SciFi", year: 2010, ratings: [9, 8, 9, 7, 8] },
+    { id: 2, title: "The Matrix", genre: "SciFi", year: 1999, ratings: [9, 8, 9, 7, 8] },
+    { id: 3, title: "Titanic", genre: "Romance", year: 1997, ratings: [7, 8, 6, 7, 8] },
+    { id: 4, title: "Avatar", genre: "SciFi", year: 2009, ratings: [8, 7, 8, 9, 7] },
+];
+
+// ---------------------------------------------------------
+
+module.exports = movies;
